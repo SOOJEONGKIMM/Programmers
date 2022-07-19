a3b9639dcdd12246d6f489c6fc846ffacbced041
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-07-20 00:13:18 +0900

    Create 기능개발.py

---
+++ 기능개발.py
@@ -0,0 +1,17 @@
+def solution(progresses, speeds):
+    res = []
+    days=0
+    cnt=0
+    while progresses:
+        if (progresses[0] + days*speeds[0])>=100:
+            cnt+=1
+            progresses.pop(0)
+            speeds.pop(0)
+        else:
+            if cnt > 0:
+                res.append(cnt)
+                cnt=0
+            days+=1
+    res.append(cnt)
+    return res
+        
