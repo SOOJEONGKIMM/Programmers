960eb6834ffc614065d7a7afe5d04b0515089540
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-07-26 00:00:46 +0900

    Create 124나라의숫자.py

---
+++ 124나라의숫자.py
@@ -0,0 +1,13 @@
+import itertools 
+def solution(n):
+    answer = ''
+    nara = ['1','2','4']
+
+
+    while n>0:
+        n-=1
+        answer = nara[n%3] + answer
+        print(answer)
+        n//=3
+
+    return answer
