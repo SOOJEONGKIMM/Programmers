a9407b99ed5c79af70240ed87aadf53c4fc1fe2b
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-07-27 14:38:11 +0900

    Create 괄호변환.py

---
+++ 괄호변환.py
@@ -0,0 +1,42 @@
+def divide(w):
+    left=0
+    right=0
+    for i in range(len(w)):
+        if w[i]=='(':
+            left+=1
+        else:
+            right+=1
+        if left==right:#균형잡힌 괄호문자열 
+            return w[:i+1], w[i+1:]
+
+def isbalanced(u):#올바른 괄호문자열인지 
+    stack = []
+    for i in u:
+        if i=='(':
+            stack.append(i)
+        else: 
+            if not stack:
+                return False
+            stack.pop() 
+    return True
+
+
+def solution(p):
+    answer = ''
+    if not p:
+        return ""
+    u, v = divide(p)
+    if isbalanced(u):
+        return u + solution(v)
+    else:
+        answer = '('
+        answer += solution(v)
+        answer += ')'
+        
+        for i in u[1:len(u)-1]:
+            if i == '(':
+                answer += ')'
+            else:
+                answer += '('
+    
+    return answer
