dfeb0a3283234187c3624d95af30ca2a725ae1fd
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-07-30 00:16:33 +0900

    Update 다리를지나는트럭.py

--- 다리를지나는트럭.py
+++ 다리를지나는트럭.py
@@ -1,1 +1,21 @@
 
+from collections import deque
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    bridge=deque([0]*bridge_length)
+    trucks = deque(truck_weights)
+    bridge_sum=0
+    while bridge:
+        p = bridge.popleft()
+        bridge_sum-=p
+        if trucks:
+            if bridge_sum+trucks[0] <= weight:
+                t = trucks.popleft()
+                bridge.append(t)
+                bridge_sum+=t
+            else:
+                bridge.append(0)
+                    
+        answer+=1
+    
+    return answer 
