fbe70bffe35ae7979b9e8777a6ccb9d65b629081
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-08-13 23:35:26 +0900

    Create 폰켓몬.py

---
+++ 폰켓몬.py
@@ -0,0 +1,10 @@
+def solution(nums):
+    answer = 0
+    take = int(len(nums)/2)
+    s = set(nums)
+    if len(s)<=take:
+        answer = len(s)
+    else:
+        answer = take
+        
+return answer
