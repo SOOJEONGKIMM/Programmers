18c3f11bd1d13f280e23b697eeede59076f41c87
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-08-24 18:34:44 +0900

    Update 올바른괄호.py

--- 올바른괄호.py
+++ 올바른괄호.py
@@ -1,1 +1,26 @@
 
+from collections import deque
+def isbalanced(w):
+    left, right =0,0
+    for i in w:
+        if i=='(':
+            left+=1
+        else:
+            right+=1
+    if left==right:
+        return True
+    else:
+        return False
+def solution(s):
+    answer = True
+    if not isbalanced(s):
+        return False
+    stack=deque()
+    for i in s:
+        if i=="(":
+            stack.append(i)
+        else:
+            if not stack:
+                return False
+            stack.popleft()
+    return True
