5bc968b88e1e9f6ddddb06ceaa6cd0f37929ef73
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-09-30 01:36:29 +0900

    Update 합승택시요금.py

--- 합승택시요금.py
+++ 합승택시요금.py
@@ -1,1 +1,20 @@
-
+def solution(n, s, a, b, fares):
+    d = [[20000001 for _ in range(n)] for _ in range(n)]
+    
+    for x in range(n):
+        d[x][x] = 0
+        
+    for x,y,c in fares:
+        d[x-1][y-1] = c
+        d[y-1][x-1] = c
+        
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                if d[j][k] > d[j][i] + d[i][k]:
+                    d[j][k] = d[j][i] + d[i][k]
+    
+    minv = 40000002
+    for i in range(n):
+        minv = min(minv, d[s-1][i]+d[i][a-1]+d[i][b-1])
+    return minv
