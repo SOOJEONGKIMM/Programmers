cbee0fd27ee06994eb66eefae603fcfd769178ca
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-11-02 18:24:35 +0900

    Update 길찾기게임.py

--- 길찾기게임.py
+++ 길찾기게임.py
@@ -1,9 +1,8 @@
 import sys
 sys.setrecursionlimit(10**6)
-def preorder(arrY, arrX, answer):
+def preorder(arrY, answer):
     cur = arrY[0]
     arrYleft, arrYright = [], []
-    idx = arrX.index(cur)
     for i in range(1, len(arrY)):
         if cur[0] > arrY[i][0]:#x축
             arrYleft.append(arrY[i])
@@ -11,37 +10,34 @@ def preorder(arrY, arrX, answer):
             arrYright.append(arrY[i])
     answer.append(cur[2])    #자기 왼 오
     if arrYleft:
-        preorder(arrYleft, arrX[:idx],answer)
+        preorder(arrYleft, answer)
     if arrYright:
-        preorder(arrYright, arrX[idx+1:],answer)  
+        preorder(arrYright, answer)  
     return 
 
-def postorder(arrY, arrX, answer):
+def postorder(arrY,answer):
     cur = arrY[0]
     arrYleft, arrYright = [], []
-    idx = arrX.index(cur)
     for i in range(1, len(arrY)):
         if cur[0] > arrY[i][0]:#x축
             arrYleft.append(arrY[i])
         else:
             arrYright.append(arrY[i])
     if arrYleft:
-        postorder(arrYleft, arrX[:idx],answer)
+        postorder(arrYleft, answer)
     if arrYright:
-        postorder(arrYright, arrX[idx+1:],answer) 
+        postorder(arrYright, answer) 
     answer.append(cur[2])   #왼 오 자기
     return 
 def solution(nodeinfo):
     answer = []
-    arrY, arrX = [], []
+    arrY = []
     pre_list, post_list = [], []
     for i in range(len(nodeinfo)):#노드번호 정보추가
         nodeinfo[i].append(i+1)
-    arrY = sorted(nodeinfo, key = lambda x: (-x[1],x[0]))
-    arrX = sorted(nodeinfo)
-    
-    preorder(arrY, arrX, pre_list)
-    postorder(arrY, arrX, post_list)
+    arrY = sorted(nodeinfo, key = lambda x: (-x[1],x[0])) 
+    preorder(arrY,  pre_list)
+    postorder(arrY, post_list)
     answer.append(pre_list)
     answer.append(post_list)
     return answer
