f49d88eb95a67850c8e060914a233e000665cf99
SOO JEONG KIM <sookelly@korea.ac.kr>
2023-01-17 22:11:45 +0900

    Update 택배배달과수거하기.py

--- 택배배달과수거하기.py
+++ 택배배달과수거하기.py
@@ -1,4 +1,14 @@
-
-
-
-
+def solution(cap, n, deliveries, pickups):
+    deliveries = deliveries[::-1]
+    pickups = pickups[::-1]
+    answer=0
+    d=0
+    p=0
+    for i in range(n):
+        d+=deliveries[i]
+        p+=pickups[i]
+        while d>0 or p>0:
+            d-=cap
+            p-=cap
+            answer+=(n-i)*2
+    return answer
