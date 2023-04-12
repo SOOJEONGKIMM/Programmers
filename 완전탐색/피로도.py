19a0a74163c2abc53a9c828f08eec7aabb040a09
SOO JEONG KIM <sookelly@korea.ac.kr>
2023-04-13 02:43:27 +0900

    Create 피로도.py

---
+++ 완전탐색/피로도.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+def solution(k, dungeons):
+    answer = -1
+    for p in permutations(dungeons, len(dungeons)):
+        tmp = k
+        cnt=0
+        for need, spend in p:
+            if tmp>=need:
+                tmp-=spend
+                cnt+=1
+        answer = max(answer, cnt)
+                
+    return answer
